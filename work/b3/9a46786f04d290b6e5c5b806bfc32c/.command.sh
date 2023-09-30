71d42518608416fe1e3825c5d5204e0af6c38832
Andrea Cossa <IEO5505@Cossas-MacBook-Pro.local>
2023-09-30 15:28:49 +0200

    real commit

--- work/b3/9a46786f04d290b6e5c5b806bfc32c/.command.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash -ue
-python /Users/IEO5505/Desktop/MI_TO/phylo_inference/bin/prep_input.py     -p /Users/IEO5505/Desktop/mito_bench/data/     --sample MDA_clones     --filtering miller2022
