c4762c6d214903b71f63952b7bbc17716a9c13e8
Andrea Cossa <IEO5505@Cossas-MacBook-Pro.local>
2024-05-09 12:13:43 +0200

    remove garbage

--- work/8d/3588866751246f6a3170cc5f62ceb6/.command.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -ue
-outfile="sAML1_job.csv"
-files=(1_job_df.csv 2_job_df.csv)
-cat "${files[0]}" > $outfile
-for f in "${files[@]:1}"; do
-    tail -n +2 "$f" >> $outfile
-done
--- work/eb/145753e8c6019ca6c45f4db82609ea/.command.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -ue
-outfile="sAML1_dataset.csv"
-files=(1_dataset_df.csv 2_dataset_df.csv)
-cat "${files[0]}" > $outfile
-for f in "${files[@]:1}"; do
-    tail -n +2 "$f" >> $outfile
-done
