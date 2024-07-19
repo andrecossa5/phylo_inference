b2464b94cbf5ac0f758d902dcb36b30aa0905e3d
Elisa Bugani <elisa.bugani@gmail.com>
2024-07-19 10:15:09 +0200

    added script to compute growth rate of clones from phylo trees using cloneRate

--- downstream/cloneRate.R
+++ downstream/cloneRate.R
@@ -7,15 +7,16 @@ library(tidyverse)
 library(parallel)
 library(Matrix)
 library(ape)
-library(PATH)
+library(cloneRate)
+library(ggplot2)
 
 
 ##
 
 
 ## 
-# path_nodes <- '/Users/IEO5505/Desktop/nodes.csv'
-# path_edges <- '/Users/IEO5505/Desktop/edges.csv'
+path_nodes <- '/Users/ieo6983/Desktop/phylo_inference/data/nodes.csv'
+path_edges <- '/Users/ieo6983/Desktop/phylo_inference/data/edges.csv'
 # path_meta <- '/Users/IEO5505/Desktop/meta.csv'
 # lineage_column <- 'aggregated_ct'
 ##
@@ -27,12 +28,77 @@ library(PATH)
 # Load data
 
 # Reconstruct tree
-nodes <- read.csv(path_nodes, row.names=1)[1:3] %>% distinct   # Avoid counting >1 nodes with >1 assigned mut
+nodes <- read.csv(path_nodes, row.names=1)[1:6] %>% distinct   # Avoid counting >1 nodes with >1 assigned mut
 edges <- read.csv(path_edges, row.names=1)
 tree <- list(
-  edge = edges %>% select(u, v) %>% as.matrix,
+  edge = edges %>% dplyr::select(u, v) %>% as.matrix,
   Nnode = sum(!is.na(nodes$support)),
   tip.label = nodes %>% arrange(node) %>% drop_na(cell) %>% .$cell,
   edge.length = edges$branch_lenghts
 )
 class(tree) <- "phylo"
+
+str(tree)
+head(nodes)
+
+
+##
+
+
+# Convert the tree to an ultrametric tree
+ultrametric_tree <- chronos(tree)
+str(ultrametric_tree)
+#plot(ultrametric_tree)
+
+
+##
+
+
+# Split clades
+node_numbers <- c(unique(nodes$clonal_ancestor)[!is.na(unique(nodes$clonal_ancestor))])
+clades <- list()
+
+for (node in node_numbers) {
+  clades[[as.character(node)]] <- extract.clade(ultrametric_tree, node)
+}
+
+# You can now access individual clades using their node numbers
+plot.phylo(clades$`302`,
+           direction = "downwards",
+           show.tip.label = T, edge.width = 1.5
+)
+axisPhylo(side = 2, backward = FALSE, las = 1)
+title(main = "Tree clone 302")
+
+
+##
+
+
+# Calculate growth rate using cloneRate main function 
+
+# Full tree
+resultsLengths <- internalLengths(ultrametric_tree)
+# Clades 
+resultsLengths <- internalLengths(clades)
+
+#' @Warning: internalLengths() is not suitable for these trees. 
+#' Using birthDeathMCMC() function is suggested 
+#' 
+#' For more @info about this: 
+#' Why birthDeathMCMC: https://cran.r-project.org/web/packages/cloneRate/vignettes/cloneRate-simulate.html 
+#' About birth-death models: https://bio.libretexts.org/Bookshelves/Evolutionary_Developmental_Biology/Phylogenetic_Comparative_Methods_(Harmon)/10%3A_Introduction_to_Birth-Death_Models 
+
+
+##
+
+
+# Calculate growth rate with birthDeathMCMC()
+results <- cloneRate::birthDeathMCMC(clades, 
+                                     maxGrowthRate = 4, # def
+                                     alpha = 0.05) # def
+View(results)
+
+
+##
+
+
