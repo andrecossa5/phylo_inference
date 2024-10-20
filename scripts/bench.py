badb64a185d6d3dd48a14c75a0c3d78265e530ff
Andrea Cossa <IEO5505@Cossas-MacBook-Pro.local>
2024-10-20 16:04:02 +0200

    final changes to scripts/bench.py

--- scripts/bench.py
+++ scripts/bench.py
@@ -5,7 +5,7 @@ import pickle
 from sklearn.metrics import silhouette_score, normalized_mutual_info_score
 from kneed import KneeLocator
 from vireoSNP import BinomMixtureVB
-from CCLONE.cluster.NMF import get_wNMF_matrices, NMF_weighted
+from CCLONE.cluster.NMF import get_wNMF_matrices, NMF_weighted, orth_score
 from mito_utils.utils import *
 from mito_utils.kNN import kNN_graph
 from mito_utils.clustering import leiden_clustering
@@ -18,7 +18,7 @@ from mito_utils.phylo import build_tree, cut_and_annotate_tree
 
 # Paths
 path_afm = '/Users/IEO5505/Desktop/MI_TO/MI_TO_analysis_repro/results/MI_TO_bench/phylo_inference/distance/MDA_PT/job2/afm.h5ad'
-maxK = 10
+maxK = 50
 
 
 ##
@@ -27,6 +27,7 @@ maxK = 10
 def main():
 
     # Read afm
+    # afm = sc.read(path_afm)
     afm = sc.read(sys.argv[1])
     maxK = int(sys.argv[2])
 
@@ -43,9 +44,12 @@ def main():
         silhouette_avg = silhouette_score(afm.obsp['distances'].A, labels, metric='precomputed')
         scores.append(silhouette_avg)
 
-    D['leiden']['labels'] = leiden_clustering(conn, res=resolutions[np.argmax(scores)])
-    D['leiden']['ARI'] = custom_ARI(afm.obs['GBC'], labels)
-    D['leiden']['NMI'] = normalized_mutual_info_score(afm.obs['GBC'], labels)
+    labels = leiden_clustering(conn, res=resolutions[np.argmax(scores)])
+    test = np.isnan(labels)
+    D['leiden']['labels'] = labels
+    D['leiden']['NA'] = test.sum() / labels.size
+    D['leiden']['ARI'] = custom_ARI(afm.obs['GBC'][~test], labels[~test])
+    D['leiden']['NMI'] = normalized_mutual_info_score(afm.obs['GBC'][~test], labels[~test])
 
 
     ## 
@@ -60,6 +64,7 @@ def main():
         _model.fit(afm.layers['AD'].T, afm.layers['DP'].T, min_iter=30, max_iter=500, max_iter_pre=250, n_init=50, random_seed=1234)
         _ELBO_mat.append(_model.ELBO_inits)
 
+    _ELBO_mat = np.row_stack(_ELBO_mat)
     x = range(2,maxK+1)
     y = np.median(_ELBO_mat, axis=1)
     knee = KneeLocator(x, y).find_knee()[0]
@@ -69,23 +74,16 @@ def main():
     _model.fit(afm.layers['AD'].T, afm.layers['DP'].T, min_iter=30, n_init=50, max_iter=500, max_iter_pre=250, random_seed=1234)
 
     clonal_assignment = _model.ID_prob
-    df_ass = pd.DataFrame(
-        clonal_assignment, 
-        index=afm.obs_names, 
-        columns=range(clonal_assignment.shape[1])
-    )
+    idx = clonal_assignment.argmax(axis=1)
+    labels = np.zeros(idx.shape)
+    for i,clone_idx in enumerate(idx):
+        labels[i] = clone_idx if clonal_assignment[i,clone_idx]>=.7 else np.nan
 
-    labels = []
-    for i in range(df_ass.shape[0]):
-        cell_ass = df_ass.iloc[i,:]
-        try:
-            labels.append(np.where(cell_ass>.7)[0][0])
-        except:
-            labels.append('unassigned')
-
+    test = np.isnan(labels)
     D['vireoSNP']['labels'] = labels
-    D['vireoSNP']['ARI'] = custom_ARI(afm.obs['GBC'], labels)
-    D['vireoSNP']['NMI'] = normalized_mutual_info_score(afm.obs['GBC'], labels)         
+    D['vireoSNP']['NA'] = test.sum() / labels.size
+    D['vireoSNP']['ARI'] = custom_ARI(afm.obs['GBC'][~test], labels[~test])
+    D['vireoSNP']['NMI'] = normalized_mutual_info_score(afm.obs['GBC'][~test], labels[~test])       
 
 
     ##
@@ -95,9 +93,12 @@ def main():
     D['MI_TO'] = {}
     tree = build_tree(afm, bin_method='vanilla', metric='jaccard', precomputed=True, solver='UPMGA')
     tree, _,_ = cut_and_annotate_tree(tree)
-    D['MI_TO']['labels'] = tree.cell_meta['MT_clone'].to_list()
-    D['MI_TO']['ARI'] = custom_ARI(tree.cell_meta['GBC'], tree.cell_meta['MT_clone'])
-    D['MI_TO']['NMI'] = normalized_mutual_info_score(tree.cell_meta.dropna()['GBC'], tree.cell_meta.dropna()['MT_clone'])      
+    labels = tree.cell_meta['MT_clone'].values
+    test = tree.cell_meta['MT_clone'].isna().values
+    D['MI_TO']['labels'] = labels
+    D['MI_TO']['NA'] = test.sum() / labels.size
+    D['MI_TO']['ARI'] = custom_ARI(afm.obs['GBC'][~test], labels[~test])
+    D['MI_TO']['NMI'] = normalized_mutual_info_score(afm.obs['GBC'][~test], labels[~test])
 
 
     ##
@@ -117,17 +118,16 @@ def main():
         afm = get_wNMF_matrices(afm, mode='10X')
         C, _ = NMF_weighted(afm, k=k, max_cycles=100, parallel=True, n_jobs=-1)
         labels = np.argmax(C, axis=1)
-        silhouette_avg = silhouette_score(afm.obsp['distances'].A, labels, metric='precomputed')
-        scores.append(silhouette_avg)
+        scores.append(orth_score(C))
 
-    k = list(range(2,maxK+1))[np.argmax(scores)]
+    k = list(range(2,maxK+1))[np.argmin(scores)]
     C, _ = NMF_weighted(afm, k=k, max_cycles=100, parallel=True, n_jobs=-1)
     labels = np.argmax(C, axis=1)
-
+    test = np.isnan(labels)
     D['CClone']['labels'] = labels
-    D['CClone']['ARI'] = custom_ARI(afm.obs['GBC'], labels)
-    D['CClone']['NMI'] = normalized_mutual_info_score(afm.obs['GBC'], labels)   
-
+    D['CClone']['NA'] = test.sum() / labels.size
+    D['CClone']['ARI'] = custom_ARI(afm.obs['GBC'][~test], labels[~test])
+    D['CClone']['NMI'] = normalized_mutual_info_score(afm.obs['GBC'][~test], labels[~test])  
     
     # Save
     with open(os.path.join(os.path.dirname(sys.argv[1]), 'bench_clonal_recontruction.pickle'), 'wb') as f:
